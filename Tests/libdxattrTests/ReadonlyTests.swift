d297f591958c6a8ef1c104cab60da3722afdb334
Aaron Rennow <arennow@outlook.com>
2026-03-07 07:26:20 -0500

    Test `noMutationsWhenReadingWithSidecar`

--- Tests/libdxattrTests/ReadonlyTests.swift
+++ Tests/libdxattrTests/ReadonlyTests.swift
@@ -15,8 +15,23 @@ struct ReadonlyTests {
 	func noMutationsWhenReadingWithoutSidecar() throws {
 		try self.fs.setWritableForTesting(at: self.fs.rootDir, writable: false)
 		var fn = FocusNode(node: self.file)
+
 		try #expect(fn.dxattrNames() == [])
 		try #expect(fn.dxattrMetadata() == [])
 		try #expect(fn.dxattrs() == [])
+	}
+
+	@Test
+	func noMutationsWhenReadingWithSidecar() throws {
+		var fn = FocusNode(node: self.file)
+		try fn.setDXAttr(name: "name", value: "value!")
+		_ = consume fn
+
+		try self.fs.setWritableForTesting(at: self.fs.rootDir, writable: false)
+		fn = FocusNode(node: self.file)
+
+		try #expect(fn.dxattrNames() == ["name"])
+		try #expect(fn.dxattrMetadata() == [DXAttrMetadata(name: "name", valueLength: 6)])
+		try #expect(fn.dxattrs() == ["name:value!"])
 	}
 }
